b34e0dbec0584738ed221ac40632483b6be28ea5
unknown <jacoluna01@gmail.com>
2024-04-24 12:34:11 -0300

    sprint 1 hecho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { WhoAmIComponent } from './componentes/who-am-i/who-am-i.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'whoAmI',  component: WhoAmIComponent },
+  {path: 'logIn', component: LoginComponent},
+  {
+    path: 'Home',
+    loadComponent: () => 
+        import('./componentes/home/home.component').then((m) => m.HomeComponent),
+  },
+
+//   { path: 'error', component: ErrorComponent },
+
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./componentes/login/login.component').then((m) => m.LoginComponent),
+  },
+
+  { path: '**', component: HomeComponent },
+];
